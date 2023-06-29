8f349d6b71479aad5df57ce2cc36af65c4313bbd
RohitBajra <104704874+rohitnewgit@users.noreply.github.com>
2023-06-29 19:33:45 +0545

    Category Model completed

--- LibraryManagementSystem/src/main/java/com/lms/controller/HomeController.java
+++ LibraryManagementSystem/src/main/java/com/lms/controller/HomeController.java
@@ -3,14 +3,16 @@ package com.lms.controller;
 import java.security.Principal;
 
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -55,8 +57,56 @@ public class HomeController {
 			session.setAttribute("message", new Message("Category Failed to Add", "danger"));
 		}
 
-		model.addAttribute("title", "LMS - Add Category");
-		return "admin/add_category";
+		model.addAttribute("title", "LMS - Show Category");
+		return "redirect:/admin/show-category/0";
+	}
+	
+	@GetMapping("/show-category/{page}")
+	public String showCategory(@PathVariable("page") Integer page,Model model) {
+		Pageable pageable = PageRequest.of(page, 5);
+		Page<Category> category = this.categoryRepository.findAllCategory(pageable);
+		model.addAttribute("title","LMS - Show Category");
+		model.addAttribute("category",category);
+		model.addAttribute("currentPage",page);
+		model.addAttribute("totalPages",category.getTotalPages());
+		return "admin/show_category";
+	}
+	
+	@PostMapping("/delete-category/{cid}")
+	public String deleteCategory(@PathVariable("cid")Integer cid,HttpSession session) {
+		try {
+			this.categoryRepository.deleteById(cid);
+			session.setAttribute("message", new Message("Category Deleted Successfully", "success"));
+		} catch (Exception e) {
+			session.setAttribute("message", new Message("Category Failed to Delete", "d"));
+		}
+		
+		return "redirect:/admin/show-category/0";
+	}
+	
+	@PostMapping("/edit-category/{cid}")
+	public String editCategory(@PathVariable("cid") Integer cid,Model model) {
+		Category category = this.categoryRepository.findById(cid).get();
+		model.addAttribute("category",category);
+		model.addAttribute("title","LMS - Edit Category");
+		return "admin/edit_category";
+	}
+	
+	@PostMapping("/update-category")
+	public String updateCategory(@ModelAttribute Category category,HttpSession session,Model model) {
+		model.addAttribute("title","LMS - Add Category");
+		Category category2 = this.categoryRepository.findById(category.getCid()).get();
+		try {
+			System.out.println("name "+category.getCategoryName()+" section "+category.getSection());
+			
+			category2.setCategoryName(category.getCategoryName());
+			category2.setSection(category.getSection());
+			this.categoryRepository.save(category2);
+			session.setAttribute("message", new Message("Category Successfully Updated", "success"));
+		} catch (Exception e) {
+			session.setAttribute("message", new Message("Category Failed to Update", "success"));
+		}
+		return "redirect:/admin/show-category/0";
 	}
 
 }
--- LibraryManagementSystem/src/main/java/com/lms/dao/CategoryRepository.java
+++ LibraryManagementSystem/src/main/java/com/lms/dao/CategoryRepository.java
@@ -1,9 +1,18 @@
 package com.lms.dao;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.lms.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+	@Query("Select c from Category c")
+	public Page<Category> findAllCategory(Pageable pageable);
+	
+
 
 }
